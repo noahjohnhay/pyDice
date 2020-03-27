7042d629c7c5c45986aa981d039f4cbcfe6a9536
Jonathan Daigle <jdaigle@liveops.com>
2020-03-26 21:33:46 -0300

    Revert "fix: formatting"

--- py_dice/__main__.py
+++ py_dice/__main__.py
@@ -1,6 +1,9 @@
 # coding=utf-8
 
 from py_dice.dice_api import routes
+from pydblite import Base
 
 if __name__ == "__main__":
+    db = Base("main.pdl")
+    db.create("game_id", mode="override")
     routes.start_api()
