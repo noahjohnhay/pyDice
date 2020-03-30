7e1f7273e07036e82ae290b686b239f8f817ae64
Noah Hay <nhay@cvent.com>
2020-03-30 18:56:12 -0300

    feat: I hope this works if not I know why

--- py_dice/common.py
+++ py_dice/common.py
@@ -2,7 +2,7 @@ import re
 from functools import reduce
 
 
-def format_dice_emojis(roll_val) -> str:
+def format_dice_emojis(roll_val: list) -> str:
     return reduce(format_dice_emoji, roll_val, "")
 
 
@@ -10,7 +10,7 @@ def format_dice_emoji(x1: str, x2: int) -> str:
     return f"{x1} :die{x2}:"
 
 
-def fetch_die_val(acc: list, x) -> list:
+def fetch_die_val(acc: list, x: dict) -> list:
     die = x["text"]["text"]
     acc.append(int(re.search(r"\d+", die).group()))
     return acc
